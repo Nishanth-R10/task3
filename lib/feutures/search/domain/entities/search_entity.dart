d1f0f4cfd46bf9fb7a24904e5882e9276b783ac0
Nishanth-R10 <nishanth.r@omnicoredigital.com>
2025-09-22 14:54:22 +0530

    Delete lib directory

--- lib/feutures/search/domain/entities/search_entity.dart
+++
@@ -1,7 +0,0 @@
-// File: lib/features/search/domain/entities/search_entity.dart
-class SearchEntity {
-  final String title;
-  final String imagePath;
-
-  SearchEntity({required this.title, required this.imagePath});
-}
--- lib/feutures/search/providers/search_provider.dart
+++
@@ -1,5 +0,0 @@
-// File: lib/core/providers/search_provider.dart
-import 'package:flutter_riverpod/legacy.dart';
-
-// For Riverpod 3.0.0
-final isReturningUserProvider = StateProvider<bool>((ref) => false);
